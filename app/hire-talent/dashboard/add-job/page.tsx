648b6f4f1192c683921026d968791ab3c1c36d96
FolashadeOmoloja <omolojashade@gmail.com>
2025-05-06 14:42:09 +0100

    activeItems Fix

--- app/hire-talent/dashboard/add-job/page.tsx
+++ app/hire-talent/dashboard/add-job/page.tsx
@@ -1,6 +1,5 @@
 import AddJobs from "@/components/Dashboard/HireTalentDashboard/AddJobs";
 import HireTalentNav from "@/components/Dashboard/HireTalentDashboard/HireTalentNav";
-import React from "react";
 
 const page = () => {
   return (
