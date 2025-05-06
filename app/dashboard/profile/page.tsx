fd2ea4543c12c482311d2aa8c83535cb07cf08c6
FolashadeOmoloja <omolojashade@gmail.com>
2025-05-06 14:14:19 +0100

    hire-talent

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -4,7 +4,7 @@ import TalentNavbar from "@/components/Dashboard/TalentDashboard/TalentNavbar";
 const page = () => {
   return (
     <>
-      <TalentNavbar activeItem={0} />
+      <TalentNavbar />
       <TalentProfile />
     </>
   );
