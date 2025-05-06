66872129932291373f6e105bd516501797aee379
FolashadeOmoloja <omolojashade@gmail.com>
2025-05-06 15:46:07 +0100

    constants Fix

--- utilities/constants/constants.ts
+++ utilities/constants/constants.ts
@@ -1,6 +1,10 @@
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
-export const TALENT_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATIONS_API_END_POINT =
-  "http://localhost:8000/api/v1/applicants";
-export const RESET_API_END_POINT = "http://localhost:8000/api/v1/reset";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api/v1"
+    : "https://talentnest-backend.onrender.com/api/v1";
+
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const TALENT_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATIONS_API_END_POINT = `${BASE_URL}/applicants`;
+export const RESET_API_END_POINT = `${BASE_URL}/reset`;
